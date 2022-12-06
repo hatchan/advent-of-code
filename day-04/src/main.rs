df280a517a009d0b33290bde88dd1fb8d9c0d5c1
Benno van den Berg <bennovandenberg@gmail.com>
2022-12-06 21:11:16 +0100

    Solve day 04 puzzle A

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -5,10 +5,44 @@ fn main() {
 }
 
 fn solve_a(input: &str) -> u64 {
-    todo!()
+    input
+        .lines()
+        .map(|line| {
+            let (left, right) = line.split_once(",").expect("no comma");
+
+            let left = Range::parse(left);
+            let right = Range::parse(right);
+
+            if (left.begin <= right.begin && left.end >= right.end)
+                || right.begin <= left.begin && right.end >= left.end
+            {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 fn solve_b(input: &str) -> u64 {
     todo!()
+}
+
+struct Range {
+    begin: u64,
+    end: u64,
+}
+
+impl Range {
+    fn new(begin: u64, end: u64) -> Self {
+        Self { begin, end }
+    }
+
+    fn parse(input: &str) -> Self {
+        let (begin, end) = input.split_once("-").expect("no dash");
+        let begin = begin.parse().expect("not a number");
+        let end = end.parse().expect("not a number");
+        Self::new(begin, end)
+    }
 }
 
 #[cfg(test)]
