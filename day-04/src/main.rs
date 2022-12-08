338ab150e102ea40f11196a720eb9e2d6667f056
Benno van den Berg <bennovandenberg@gmail.com>
2022-12-08 21:45:00 +0100

    Slightly optimize code

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -4,36 +4,30 @@ fn main() {
     println!("Day 04 B: {}", solve_b(input));
 }
 
-fn solve_a(input: &str) -> u64 {
+fn solve_a(input: &str) -> usize {
     input
         .lines()
-        .map(|line| {
+        .filter(|line| {
             let (left, right) = line.split_once(",").expect("no comma");
 
             let left = Range::parse(left);
             let right = Range::parse(right);
 
-            if (left.begin <= right.begin && left.end >= right.end)
-                || right.begin <= left.begin && right.end >= left.end
-            {
-                1
-            } else {
-                0
-            }
+            left.contains(&right) || right.contains(&left)
         })
-        .sum()
+        .count()
 }
-fn solve_b(input: &str) -> u64 {
+fn solve_b(input: &str) -> usize {
     todo!()
 }
 
 struct Range {
-    begin: u64,
-    end: u64,
+    begin: usize,
+    end: usize,
 }
 
 impl Range {
-    fn new(begin: u64, end: u64) -> Self {
+    fn new(begin: usize, end: usize) -> Self {
         Self { begin, end }
     }
 
@@ -42,6 +36,10 @@ impl Range {
         let begin = begin.parse().expect("not a number");
         let end = end.parse().expect("not a number");
         Self::new(begin, end)
+    }
+
+    fn contains(&self, other: &Self) -> bool {
+        self.begin <= other.begin && self.end >= other.end
     }
 }
 
@@ -58,7 +56,6 @@ mod tests {
 
     #[test]
     fn solve_b_success() {
-        panic!();
-        // assert_eq!(solve_b(INPUT), 70);
+        assert_eq!(solve_b(INPUT), 4);
     }
 }
