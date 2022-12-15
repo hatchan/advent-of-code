7981cec99a0db5519387712e85ea202cb2b86612
Benno van den Berg <bennovandenberg@gmail.com>
2022-12-15 16:04:35 +0100

    Solve day 10 puzzle A

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,20 +1,79 @@
 fn main() {
     let input = include_str!("../input.txt");
     println!("Day 10 A: {}", solve_a(input));
-    println!("Day 10 B: {}", solve_b(input));
+    // println!("Day 10 B: {}", solve_b(input));
 }
 
-fn solve_a(input: &str) -> usize {
-    todo!()
+fn solve_a(input: &str) -> isize {
+    let mut instructions = input.lines().map(Instruction::parse);
+
+    let mut result = 0;
+
+    let mut current_cycle = 1;
+
+    let mut current_instruction = None;
+    let mut cycle_remaining = 1;
+    let mut current_state = 1;
+
+    loop {
+        if cycle_remaining == 0 {
+            if let Some(instruction) = current_instruction.take() {
+                match instruction {
+                    Instruction::Noop => {}
+                    Instruction::Add(value) => {
+                        current_state += value;
+                    }
+                }
+            }
+        }
+
+        if (current_cycle - 20) % 40 == 0 {
+            println!("{}: {} ({})", current_cycle, current_state, result);
+            result += current_state * current_cycle;
+        }
+
+        if current_instruction.is_none() {
+            match instructions.next() {
+                Some(instruction) => {
+                    cycle_remaining = instruction.cycles();
+                    current_instruction = Some(instruction);
+                }
+                None => break,
+            };
+        }
+
+        cycle_remaining -= 1;
+        current_cycle += 1;
+    }
+
+    result
 }
 
-fn solve_b(input: &str) -> usize {
+fn solve_b(_input: &str) -> usize {
     todo!()
 }
 
 enum Instruction {
     Noop,
-    Addx(isize),
+    Add(isize),
+}
+
+impl Instruction {
+    fn parse(input: &str) -> Self {
+        if input == "noop" {
+            Instruction::Noop
+        } else {
+            let (_, value) = input.split_once(" ").unwrap();
+            Instruction::Add(value.parse().unwrap())
+        }
+    }
+
+    fn cycles(&self) -> usize {
+        match self {
+            Instruction::Noop => 1,
+            Instruction::Add(_) => 2,
+        }
+    }
 }
 
 #[cfg(test)]
