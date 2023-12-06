a6158d6535872d4da52d9cdcfa1902d2efa85e97
Benno van den Berg <bennovandenberg@gmail.com>
2023-12-06 13:57:10 +0100

    Solve day 04-B

--- 2023/day-04/src/main.rs
+++ 2023/day-04/src/main.rs
@@ -1,7 +1,9 @@
+use std::collections::{HashSet, VecDeque};
+
 fn main() {
     let input = include_str!("../input.txt");
     println!("Day 04 A: {}", solve_a(input));
-    // println!("Day 04 B: {}", solve_b(input));
+    println!("Day 04 B: {}", solve_b(input));
 }
 
 fn solve_a(input: &str) -> usize {
@@ -19,14 +21,42 @@ fn calc_winnings(input: &str) -> usize {
         .count();
 
     if wins == 0 {
-        return 0;
+        0
     } else {
         2_usize.pow((wins - 1) as u32)
     }
 }
 
 fn solve_b(input: &str) -> usize {
-    todo!("solve_b")
+    let mut copies = 0;
+    let mut look_ahead: VecDeque<usize> = VecDeque::new();
+
+    for line in input.lines() {
+        let (_card, content) = line.split_once(": ").expect("invalid format");
+        let (winnings, numbers) = content.split_once(" | ").expect("invalid format");
+        let winnings: Vec<_> = winnings.split(' ').filter(|w| !w.is_empty()).collect();
+
+        let wins = numbers
+            .split(' ')
+            .filter(|n| !n.is_empty() && winnings.contains(n))
+            .count();
+
+        // Take the copies that we accumulated so far and add one for the current card
+        let total_cards = look_ahead.pop_front().unwrap_or_default() + 1;
+
+        for i in 0..wins {
+            match look_ahead.get_mut(i) {
+                Some(look_ahead_number) => *look_ahead_number += total_cards,
+                None => {
+                    look_ahead.push_back(total_cards);
+                }
+            }
+        }
+
+        copies += total_cards;
+    }
+
+    copies
 }
 
 #[cfg(test)]
@@ -40,8 +70,8 @@ mod tests {
         assert_eq!(solve_a(INPUT), 13);
     }
 
-    // #[test]
-    // fn solve_b_success() {
-    //     assert_eq!(solve_b(INPUT), 0);
-    // }
+    #[test]
+    fn solve_b_success() {
+        assert_eq!(solve_b(INPUT), 30);
+    }
 }
